1138f77c0f33939ed37da261577327f41b16006f
Ибрахим Магомаев <ibrahimmagomaev@Ibrahims-MacBook-Air-2.local>
2026-02-03 02:15:05 +0400

    feat: add vercel analytics

--- minimalist-portfolio/src/App.tsx
+++ minimalist-portfolio/src/App.tsx
@@ -4,6 +4,8 @@ import { Story } from './components/Story';
 import { Footer } from './components/Footer';
 import { StickyHeader } from './components/StickyHeader';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App() {
   return (
     <div className="relative min-h-screen bg-bg-primary text-text-primary selection:bg-brand-accent selection:text-white overflow-x-hidden font-body flex flex-col items-center">
@@ -19,6 +21,7 @@ export default function App() {
 
       {/* Footer / Waitlist Section */}
       <Footer />
+      <Analytics />
     </div>
   );
 }
